af80f797d01bb0f451cd6625bf7e3db8195ef5b0
Tomasz <tomasz.sulek90@gmail.com>
2016-09-04 13:50:13 +0200

    Home Controller change

--- src/main/java/com/packt/webstore/controller/HomeController.java
+++ src/main/java/com/packt/webstore/controller/HomeController.java
@@ -10,7 +10,7 @@ public class HomeController {
 	@RequestMapping("/")
 	public String welcome(Model model){
 		model.addAttribute("greeting", "Welcome to our store!");
-		model.addAttribute("tagline", "Wyjątkowym i elegankim sklepie");
+		model.addAttribute("tagline", "Special and smart store");
 		return "welcome";
 	}
 }
